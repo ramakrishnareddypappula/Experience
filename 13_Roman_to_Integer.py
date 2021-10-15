e33eaa4599518b6b822be3e41b4960bdb4e0c23c
rampappula@gmail.com <rampappula@gmail.com>
2021-10-14 22:58:19 -0700

    Roman to integer.

---
+++ 13_Roman_to_Integer.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        d = {"I": 1, "V": 5, "X": 10, "L" : 50, "C" : 100, "D": 500, "M":1000}
+        sum1 = 0
+        for i in range(len(s) - 1):
+            if d[s[i]] < d[s[i+1]]:
+                sum1 -= d[s[i]]
+            else:
+                sum1 += d[s[i]]
+        return sum1 + d[s[-1]]
